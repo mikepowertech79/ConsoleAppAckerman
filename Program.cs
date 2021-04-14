4f644920ca285b2bdbe678e302e10745ce888041
mikepowertech79 <mikepowertech79@gmail.com>
2021-04-14 16:39:13 +0800

    ok

--- Program.cs
+++ Program.cs
@@ -11,11 +11,18 @@ namespace ConsoleAppAckerman
         static void Main(string[] args)
         {
 
-                for (int i = 0; i < 6; i++)
+
+            // Console.WriteLine(ack1(1, 2));
+
+
+
+
+
+            for (int i = 0; i < 6; i++)
                 for (int j = 0; j < 6; j++)
                     Console.WriteLine($"ackerman ({i},{j}) is:{ack(i, j)}");
-                Console.ReadLine();
-        
+            Console.ReadLine();
+
 
         }
 
@@ -26,6 +33,25 @@ namespace ConsoleAppAckerman
             else if (n == 0) answer = ack(m - 1, 1);
             else answer = ack(m - 1, ack(m, n - 1));
             return answer;
+        }
+
+
+        static int ack1(int m, int n)
+        {
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            else if ((m > 0) && (n == 0))
+            {
+                return ack1(m - 1, 1);
+            }
+            else if ((m > 0) && (n > 0))
+            {
+                return ack1(m - 1, ack1(m, n - 1));
+            }
+            else
+                return n + 1;
         }
     }
 }
